cd7221d7816c84d8c2d9d3873a6530ecfa81d38a
Montague, Brent <brent@bmontague.com>
2026-05-02 12:51:26 -0400

    feat: add time subcommand with --utc flag

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,50 @@
-use clap::Parser;
+use chrono::{Local, Utc};
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args {
-  #[arg(short, long)]
-  debug: bool,
+struct Cli {
+    /// Enable debug output
+    #[arg(short, long)]
+    debug: bool,
+
+    #[command(subcommand)]
+    command: Commands,
 }
 
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Print the current time
+    Time(TimeArgs),
+}
+
+#[derive(Args, Debug)]
+struct TimeArgs {
+    /// Also print UTC time
+    #[arg(long)]
+    utc: bool,
+}
+
+fn format_time<Tz: chrono::TimeZone>(dt: chrono::DateTime<Tz>) -> String
+where
+    Tz::Offset: std::fmt::Display,
+{
+    dt.format("%A %B %-d %Y %I:%M %p").to_string()
+}
 
 fn main() {
-  let args = Args::parse();
+    let args = Cli::parse();
 
-  if args.debug {
-    println!("Debug mode is on");
-  } else {
-    println!("Debug mode is off");
-  }
+    if args.debug {
+        println!("Debug mode is on");
+    }
+
+    match args.command {
+        Commands::Time(time_args) => {
+            println!("{}", format_time(Local::now()));
+            if time_args.utc {
+                println!("UTC: {}", format_time(Utc::now()));
+            }
+        }
+    }
 }
